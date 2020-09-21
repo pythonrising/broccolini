06601759b915578c7d11a3fb65bbb222c299fa1e
Gregory Wendel <gwendel@gmail.com>
2020-09-21 09:46:27 -0400

    updates

--- tests/integration_tests/test_files_to_fauna_integration.py
+++ tests/integration_tests/test_files_to_fauna_integration.py
@@ -35,7 +35,7 @@ class TestIntegrationFileToFauna:
             raise ValueError("Missing environment variables") from _error
 
     # @pytest.mark.skip(reason="integration testing")
-    # @pytest.fixture()
+    @pytest.fixture()
     def test_get_files_from_folder(self, return_data_dict):
         """Get list of files from the directory.
 
@@ -51,7 +51,7 @@ class TestIntegrationFileToFauna:
         # logging.debug(type(result[0]['folders_and_files'][0]))
         return result
 
-    # @pytest.mark.skip(reason="integration testing")
+    @pytest.mark.skip(reason="integration testing")
     def test_write_files_to_fauna_db(self, return_data_dict, return_random_uuid, test_get_files_from_folder):
         """Put list of files into fauna database
 
@@ -63,44 +63,23 @@ class TestIntegrationFileToFauna:
         collection_name: files
         index on filename
         subject itstuff
+        create the collection
+        then add data
+        data add works
+        first need to get the data into a serializable format
+        start with just a list of the data to get a record into the database
+        then do the cleanup and addition
         """
         collection_name = f"collection_{return_random_uuid}"
         client_token = TestIntegrationFileToFauna.get_test_values(return_data_dict["fauna_secret_path_server"])
-        records_to_add = test_get_files_from_folder
-        # result = DataBaseOperations(client_token=client_token).fauna_add_records(
-        #     collection_name=collection_name,
-        #     records_to_add=records_to_add,
-        # )
-        logging.debug(records_to_add)
-        # logging.debug(client_token)
+        # records_to_add = test_get_files_from_folder  # NEED TO FIX THE DATA BEFORE IMPORT
+        records_to_add=return_data_dict["fauna_test_data"]
 
-
-#     def get_test_values(cls, secret_path):
-#         """Build values needed for the test."""
-#         try:
-#             fauna_secret_key = VaultFunctions().query_vault_data(
-#                 vault_url="VAULT_URL",
-#                 vault_token="VAULT_TOKEN",
-#                 secret_path=secret_path,
-#             )
-#             return fauna_secret_key["data"]["data"]["_key"]
-#         except KeyError as _error:  # pragma: no cover
-#             raise ValueError("Missing environment variables") from _error
-
-# # get the
-#  result = FileOperationFunctions().get_file_information_build(
-#             input_directory=return_data_dict["faker_files"],
-#             output_file_name=return_data_dict["output_file_name"],
-#         )
-
-# # will need fauna write to database since collection and database are alreaedy existing
-# # database in use will be from the token value
-
-# # first get the listt of files
-
-
-# # call functions to combine the functions together
-# # first file operations to build list of files
-# # then database to write them to databasea
-# # then notification
-# #directory will be the training test database
+        DataBaseOperations(client_token=client_token).fauna_create_collection(
+            collection_name=collection_name,
+        )
+        result = DataBaseOperations(client_token=client_token).fauna_add_records(
+            collection_name=collection_name,
+            records_to_add=records_to_add,
+        )
+        logging.debug(result)
