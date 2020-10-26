185d5683800268d470f773bcab27326c15a636f6
Gregory Wendel <gwendel@gmail.com>
2020-10-26 01:10:24 -0400

    working with test data and dataclasses

--- tests/integration_tests/test_files_to_fauna_integration.py
+++ tests/integration_tests/test_files_to_fauna_integration.py
@@ -9,11 +9,22 @@ Also used as a basis for testing file async functionality
 
 import logging
 
+from dataclasses import dataclass
+
 import pytest
 
 from broccolini.authentication_functions import VaultFunctions
 from broccolini.database_operations import DataBaseOperations
 from broccolini.fileoperation_functions import FileOperationFunctions
+
+
+@dataclass
+class FileAndFolderInformation:
+    """File and folder storage"""
+
+    file_name: str
+    path: str
+    subject: str = None
 
 
 FAUNA_COLLECTION_NAME = "training_collection"
@@ -59,30 +70,55 @@ class TestIntegrationFileToFauna:
             input_directory=return_data_dict["faker_files"],
             output_file_name=return_data_dict["output_file_name"],
         )
-        # print(result)
         return result
-
-    # self, return_database_settings, return_random_uuid, test_get_files_from_folder,
-    # @pytest.mark.skip(reason="integration testing")
 
     @staticmethod
     def test_integration_sample_training_data_to_fauna(
         return_database_settings, test_get_files_from_folder
     ):
-        """Full fauna test with existing collection and database."""
+        """Create document using data created in other function."""
         client_token = TestIntegrationFileToFauna.get_test_values(
             return_database_settings["fauna_path_srv"],
         )
-        print(test_get_files_from_folder)
 
-        list_data_for_fauna = [
-            "tests\\fake_data_from_conftest\\training\\javascript\\behavior.pdf",
-            "seconddasdfasdfata",
-        ]
-        records_to_add = {"data": {"name": list_data_for_fauna}}
+        # run this if you manually delete the collection
+        # DataBaseOperations(client_token=client_token).fauna_create_collection(
+        #     fauna_collection_name=FAUNA_COLLECTION_NAME,
+        # )
+        list_of_dataclasses = []
 
-        result = DataBaseOperations(client_token=client_token).fauna_create_document(
-            fauna_collection_name=FAUNA_COLLECTION_NAME,
-            fauna_document_data=records_to_add,
-        )
-        return result
+        for each in test_get_files_from_folder:
+            file_name = each["folders_and_files"][0].name
+            path = str(each["folders_and_files"][0].parent)
+            subject = "subject_needed"
+            dataclass_ = FileAndFolderInformation(file_name, path, subject)
+            list_of_dataclasses.append(dataclass_)
+            # file_data2 = FileAndFolderInformation(file_name, path, subject)
+
+        # print(list_of_dataclasses)
+        for each in list_of_dataclasses:
+            records_to_add = {
+                "data": {
+                    "file_name": each.file_name,
+                    "path": each.path,
+                    "subject": each.subject,
+                }
+            }
+            print(records_to_add)
+            DataBaseOperations(client_token=client_token).fauna_create_document(
+                fauna_collection_name=FAUNA_COLLECTION_NAME,
+                fauna_document_data=records_to_add,
+            )
+
+        # records_to_add = {"data": {
+        #     "file_name": file_data2.file_name,
+        #     "path": file_data2.path,
+        #     "subject": file_data2.subject,
+        #     }
+        # }
+
+        # result = DataBaseOperations(client_token=client_token).fauna_create_document(
+        #     fauna_collection_name=FAUNA_COLLECTION_NAME,
+        #     fauna_document_data=records_to_add,
+        # )
+        # return result
