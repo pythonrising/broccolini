bfcc285e0fccf2b7d9d5196a38e7c867dd7ee999
Gregory Wendel <gwendel@gmail.com>
2020-11-02 21:15:42 -0500

    updated for pylint

--- tests/test_presentation_functions.py
+++ tests/test_presentation_functions.py
@@ -29,7 +29,6 @@ class TestPresentationOperations:
         result = PresentationOperations().prepare_template(
             input_data_file=presentation_settings["input_data_file"],
             input_template_name=presentation_settings["input_template_name"],
-            output_file_name_jinja2=presentation_settings["output_file_name_jinja2"],
             template_folder=presentation_settings["template_folder"],
             trim_blocks=presentation_settings["trim_blocks"],
             lstrip_blocks=presentation_settings["lstrip_blocks"],
@@ -37,5 +36,4 @@ class TestPresentationOperations:
             autoescape_formats=presentation_settings["autoescape_formats"],
         )
         expected_type = tuple
-        # print(result)
         assert isinstance(result, expected_type)
