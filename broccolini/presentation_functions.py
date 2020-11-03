969e3fcd8facdb9f7a0d68490172f758184bec46
Gregory Wendel <gwendel@gmail.com>
2020-11-03 13:08:15 -0500

    update presentation functions

--- broccolini/presentation_functions.py
+++ broccolini/presentation_functions.py
@@ -4,6 +4,8 @@ Presentation operations.
 """
 import logging
 
+from pathlib import Path
+
 from jinja2 import Environment
 from jinja2 import FileSystemLoader
 from jinja2 import select_autoescape
@@ -26,31 +28,39 @@ class PresentationOperations:
         return f"{class_name}"
 
     @staticmethod
-    def prepare_template(**kwargs: str) -> tuple[bool, str]:
-        """Create template.
-        input_data_file: file to build dictionary from
-        input_template_name: str name of the template file
-        output_file_name_jinja2: str - output_file_name_jinja2
+    def prepare_template_with_pathlib(
+        **kwargs: str,
+    ) -> tuple[bool, str]:
+        """Given a path to templates, a file name and a dictionary
+
+        Prepare a final template
+        examples input_dictionary: dict[str, object], input_template: str,
+
+        Args: (called from *kwargs)
+            input_dictionary (dict[str, object]): [description]
+            input_template_file_name (str): template used to build the output
+            output_text (str): output text
+
+        Returns:
+            tuple[bool, str]: true for success, and the name of the output_file_name
+
         """
-        input_data_file: str = kwargs["input_data_file"]
-        input_template_name: str = kwargs["input_template_name"]
-        # output_file_name_jinja2: str = kwargs["output_file_name_jinja2"]
-        template_folder: str = kwargs["template_folder"]
-        file_loader: str = FileSystemLoader(template_folder)
+        input_dictionary = kwargs["input_dictionary"]
+        input_template_name = kwargs["input_template_name"]
+        # file_loader: str = FileSystemLoader(template_folder)
         trim_blocks: str = kwargs["trim_blocks"]
         lstrip_blocks: str = kwargs["lstrip_blocks"]
         keep_trailing_newline: str = kwargs["keep_trailing_newline"]
         autoescape_formats: str = kwargs["autoescape_formats"]
 
+        path_obj = Path(__file__).parent.parent / "templates"  # sample relative path
         env = Environment(
-            loader=file_loader,
+            loader=FileSystemLoader(Path(path_obj)),
             trim_blocks=trim_blocks,
             lstrip_blocks=lstrip_blocks,
             keep_trailing_newline=keep_trailing_newline,
             autoescape=select_autoescape(autoescape_formats),
         )
         template = env.get_template(input_template_name)
-        output = template.render(jinja_var=input_data_file)
-        # with open(output_file_name_jinja2, "w") as file_handle:
-        #     file_handle.write(output)
+        output = template.render(jinja_var=input_dictionary)
         return True, output
