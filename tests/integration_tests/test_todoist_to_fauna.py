b61b6d1671c1ab8ab6bd69bfa46dd74ebf20c2d9
Gregory Wendel <gwendel@gmail.com>
2020-11-04 20:43:47 -0500

    working with getting date and len of item

--- tests/integration_tests/test_todoist_to_fauna.py
+++ tests/integration_tests/test_todoist_to_fauna.py
@@ -7,6 +7,7 @@ processing: generate data into a dictionary
 put this data into fauna with a timestamp
 """
 
+import datetime
 import logging
 
 from broccolini.authentication_functions import VaultFunctions
@@ -14,6 +15,8 @@ from broccolini.todoist_operations import TodoIstOperations
 
 
 SECRET_PATH = "python_rising/dev/todoist_data/TODOIST_API_TOKEN"
+FAUNA_DATABASE_NAME = "todoist_archives"
+TODAYS_DATE = (datetime.date(2019, 10, 20)).strftime("%Y%m%d")
 
 logging.basicConfig(
     level=logging.DEBUG, format=" %(asctime)s - %(levelname)s - %(message)s"
@@ -38,126 +41,20 @@ class TestIntegrationTodoistFauna:
             raise ValueError("Missing environment variables") from _error
 
     @staticmethod
+    # @pytest.mark.skip(reason="integration testing")
     def test_integration_todoist_to_fauna():
-        """XXXa."""
+        """Test the integration from todoist to fauna."""
+
         todoist_api_token = TestIntegrationTodoistFauna.get_test_values(
             SECRET_PATH,
         )
-        result = TodoIstOperations().list_projects(
+        todoist_items = TodoIstOperations().list_items(
             todoist_api_token=todoist_api_token,
         )
-        print(result)
-
-        expected_type = list
-        assert isinstance(result, expected_type)
-
-        # DataBaseOperations(client_token=client_token).fauna_create_document(
-        #         fauna_collection_name=FAUNA_COLLECTION_NAME,
-        #         fauna_document_data=records_to_add,
-        #     )
-
-
-# class TestIntegrationTodoistToFauna:
-#     """Test the integration from files to fauna database."""
-
-#     @classmethod
-#     def get_test_values(cls, secret_path):
-#         """Build values needed for the test."""
-#         try:
-#             fauna_secret_key = VaultFunctions().query_vault_data(
-#                 vault_url="VAULT_URL",
-#                 vault_token="VAULT_TOKEN",
-#                 secret_path=secret_path,
-#             )
-#             return fauna_secret_key["data"]["data"]["_key"]
-#         except KeyError as _error:  # pragma: no cover
-
-#             raise ValueError("Missing environment variables") from _error
-
-#     def test_integration_todoist_data_to_fauna():
-#         """Create document from data created in other function."""
-#         # client_token = TestIntegrationTodoistToFauna.get_test_values(
-#         #     secret_path=SECRET_PATH,
-#         #     )
-#         try:
-#             client_token = TestIntegrationTodoistToFauna.get_test_values(
-#             secret_path=SECRET_PATH,
-#             )
-#             print(len(client_token))
-
-#         except Exception as _error:  # pragma: no cover
-#             print(f'error is {_error}')
-
-# # def test_integration_sample_training_data_to_fauna(
-# #         return_database_settings, test_get_files_from_folder
-# #     ):
-# #         """Create document using data created in other function."""
-# #         client_token = TestIntegrationFileToFauna.get_test_values(
-# #             return_database_settings["fauna_path_srv"],
-# #         )
-
-#         # client_token = TestIntegrationTodoistToFauna.get_test_values(
-#         #     secret_path=SECRET_PATH,
-#         # )
-#         # print(len(client_token))
-#         # TodoIstOperations(client_token=client_token).list_projects()
-
-# # FAUNA_DATABASE_NAME = 'todoist_archives'
-
-# # def vault_login(secret_path):
-# #     """login to vault
-
-# #     Args:
-# #         secret_path ([str]): path in vault
-
-# #     Raises:
-# #         ValueError: error if vault issue
-
-# #     Returns:
-# #         vault_path: path to use from vault
-# #         result = VaultFunctions.get_vault_credentials(
-# #             vault_url="VAULT_URL", vault_token="VAULT_TOKEN"
-# #         )
-# #     """
-# #     try:
-# #         secret_key = VaultFunctions().query_vault_data(
-# #             vault_url="VAULT_URL",
-# #             vault_token="VAULT_TOKEN",
-# #             secret_path=secret_path,
-# #         )
-# #         return secret_key["data"]["data"]["_key"]
-# #     except KeyError as _error:  # pragma: no cover
-# #         raise ValueError("Missing environment variables") from _error
-
-# # # @pytest.mark.skip(reason="integration testing")
-# # def test_integrations_get_todoist_projects():
-# #     """Test login to fauna.
-
-# #     input: fauna projects
-# #     input: secret_path
-# #     input: fauna database name to use
-# #     side_effect: fauna db write
-# #     output: list of data updated -or- bool
-# #     input: vault credentials
-# #     input_type: str
-# #     output_type: FaunaClient
-# #     output example: <faunadb.client.FaunaClient object at 0x000002439xxxxx>
-# #     """
-
-# #     try:
-# #         todoist_api_token = vault_login(
-# #             secret_path=SECRET_PATH,
-# #         )
-
-# #         todoist_project_data = TodoIstOperations().list_projects(
-# #             todoist_api_token=todoist_api_token,
-# #         )
-# #         return todoist_project_data
-
-# #     except Exception as _error:  # pragma: no cover
-# #         raise ValueError("Todoist error.") from _error
-
-
-# #     # next take project data and add date and timestamp
-# #     # using new python date timestamp functionailty
-# #     # keep in mind javascript / fauna limitations
+        # add todoist items content to new list_items
+        list_of_items = []
+        # todays_date = (datetime.date(2019, 10, 20)).strftime("%Y-%m-%d")
+        # dt.strftime("%d-%m-%Y")
+        print(TODAYS_DATE)
+        for each in todoist_items:
+            list_of_items.append(len(each["content"]))
