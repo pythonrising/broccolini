80d031cadf694bc9d67f7232bd0693c770a43aa3
Gregory Wendel <gwendel@gmail.com>
2020-11-15 20:51:45 -0500

    replaced class function with config for todoist

--- tests/test_todoist_functions.py
+++ tests/test_todoist_functions.py
@@ -11,7 +11,8 @@ import pytest
 
 from todoist import TodoistAPI
 
-from broccolini.authentication_functions import VaultFunctions
+# from broccolini.authentication_functions import VaultFunctions
+from broccolini.common import get_authentication_values
 from broccolini.todoist_operations import TodoIstOperations
 
 
@@ -23,24 +24,27 @@ logging.basicConfig(
 class TestTodoIstOperations:
     """Test Todoist Functions."""
 
-    @classmethod
-    def get_test_values(cls, secret_path):
-        """Build values for the test."""
-        try:
-            todoist_secret_key = VaultFunctions().query_vault_data(
-                vault_url="VAULT_URL",
-                vault_token="VAULT_TOKEN",
-                secret_path=secret_path,
-            )
-            return todoist_secret_key["data"]["data"]["_key"]
-        except KeyError as _error:  # pragma: no cover
-            raise ValueError("Missing environment variables") from _error
+    # @classmethod
+    # def get_test_values(cls, secret_path):
+    #     """Build values for the test."""
+    #     try:
+    #         todoist_secret_key = VaultFunctions().query_vault_data(
+    #             vault_url="VAULT_URL",
+    #             vault_token="VAULT_TOKEN",
+    #             secret_path=secret_path,
+    #         )
+    #         return todoist_secret_key["data"]["data"]["_key"]
+    #     except KeyError as _error:  # pragma: no cover
+    #         raise ValueError("Missing environment variables") from _error
 
     @staticmethod
     @pytest.mark.dependency(name="test_login_to_todoist")
     def test_todoist_get_connection(return_data_dict):
         """Connect to todoist test."""
-        todoist_api_token = TestTodoIstOperations.get_test_values(
+        # todoist_api_token = TestTodoIstOperations.get_test_values(
+        #     secret_path=return_data_dict["todoist_secret_path"]
+        # )
+        todoist_api_token = get_authentication_values(
             secret_path=return_data_dict["todoist_secret_path"]
         )
 
@@ -53,10 +57,13 @@ class TestTodoIstOperations:
     @staticmethod
     def test_todoist_list_items(return_data_dict):
         """Test todoist list tasks."""
-        todoist_api_token = TestTodoIstOperations.get_test_values(
+        todoist_api_token = get_authentication_values(
             secret_path=return_data_dict["todoist_secret_path"]
         )
 
+        # todoist_api_token = TestTodoIstOperations.get_test_values(
+        #     secret_path=return_data_dict["todoist_secret_path"]
+        # )
         result = TodoIstOperations().list_items(
             todoist_api_token=todoist_api_token,
         )
@@ -66,10 +73,12 @@ class TestTodoIstOperations:
     @staticmethod
     def test_todoist_list_projects(return_data_dict):
         """Test todoist list projects."""
-        todoist_api_token = TestTodoIstOperations.get_test_values(
+        # todoist_api_token = TestTodoIstOperations.get_test_values(
+        #     secret_path=return_data_dict["todoist_secret_path"]
+        # )
+        todoist_api_token = get_authentication_values(
             secret_path=return_data_dict["todoist_secret_path"]
         )
-
         result = TodoIstOperations().list_projects(
             todoist_api_token=todoist_api_token,
         )
@@ -79,7 +88,10 @@ class TestTodoIstOperations:
     @staticmethod
     def test_filter_items(return_data_dict):
         """Test todoist filter items."""
-        todoist_api_token = TestTodoIstOperations.get_test_values(
+        # todoist_api_token = TestTodoIstOperations.get_test_values(
+        #     secret_path=return_data_dict["todoist_secret_path"]
+        # )
+        todoist_api_token = get_authentication_values(
             secret_path=return_data_dict["todoist_secret_path"]
         )
 
